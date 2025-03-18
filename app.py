bee481bd0c0706ff80130645216287b50aef6abe
musafali049 <musafali049@gmail.com>
2025-03-18 07:32:40 -0700

    Updated app.py to run Flask and Gradio together on different ports

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import threading
 app = Flask(__name__)
 
 # Load the trained model (update path if needed)
-MODEL_PATH = "model/model_weights.h5"
+MODEL_PATH = "model_weights.h5"
 model = tf.keras.models.load_model(MODEL_PATH)
 
 # Define the class labels (ensure this matches your training setup)
